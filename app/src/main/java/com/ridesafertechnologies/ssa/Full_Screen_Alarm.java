aaae1101803da3d8bc915f9dc035d0daad83eeaf
Ryan Mainard <rmainard007@gmail.com>
2015-03-05 00:30:44 -0800

    Full screen alarm audio

--- app/src/main/java/com/ridesafertechnologies/ssa/Full_Screen_Alarm.java
+++ app/src/main/java/com/ridesafertechnologies/ssa/Full_Screen_Alarm.java
@@ -1,6 +1,7 @@
 package com.ridesafertechnologies.ssa;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
@@ -10,17 +11,25 @@ import android.view.View;
 
 public class Full_Screen_Alarm extends ActionBarActivity {
     private boolean close = false;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_full__screen__alarm);
+        mediaPlayer = MediaPlayer.create(this,R.raw.tornadosireniidelilah747233690 );
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
+
     }
     //use another intent to close the full screen alarm activity
     @Override
     protected void onNewIntent(Intent intent){
         super.onNewIntent(intent);
         close = intent.getExtras().getBoolean("close");
+
         if(close = true){
+            mediaPlayer.release();
+            mediaPlayer = null;
             this.finish();
         }
     }
@@ -49,10 +58,15 @@ public class Full_Screen_Alarm extends ActionBarActivity {
     public void dismiss(View view){
         //it only hides the activity and waits to be killed by the
         //notification trigger.
+        mediaPlayer.release();
+        mediaPlayer = null;
         this.moveTaskToBack(true);
+
+        //this.finish();
     }
     public void snooze(View view) {
         //send intent to snooze service
+        mediaPlayer.stop();
         this.moveTaskToBack(true);
         //need the snooze service to bring it back
     }
