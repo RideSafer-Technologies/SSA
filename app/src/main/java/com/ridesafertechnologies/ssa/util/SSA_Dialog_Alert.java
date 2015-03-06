d694da5f8f923288117182b26949996df4d9b2ba
Ryan Mainard <rmainard007@gmail.com>
2015-03-05 19:20:47 -0800

    Dialog Alert has been created, the Full Screen alarm needed a little tweaking.

--- app/src/main/java/com/ridesafertechnologies/ssa/util/SSA_Dialog_Alert.java
+++ app/src/main/java/com/ridesafertechnologies/ssa/util/SSA_Dialog_Alert.java
@@ -1,5 +1,7 @@
 package com.ridesafertechnologies.ssa.util;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -9,18 +11,23 @@ import com.ridesafertechnologies.ssa.R;
 
 public class SSA_Dialog_Alert extends ActionBarActivity {
 
+    private boolean close = false;
+    private MediaPlayer mediaPlayer = new MediaPlayer();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ssa__dialog__alert);
+        mediaPlayer = MediaPlayer.create(this, R.raw.tornadosireniidelilah747233690);
+        mediaPlayer.setLooping(true);
+        mediaPlayer.start();
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_ssa__dialog__alert, menu);
-        return true;
+        //getMenuInflater().inflate(R.menu.menu_ssa__dialog__alert, menu);
+        return false;
     }
 
     @Override
@@ -36,5 +43,30 @@ public class SSA_Dialog_Alert extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+    //Close SSA Dialog Alert if with an intent, or resume it after
+    //snooze
+    protected void onNewIntent(Intent intent){
+        super.onNewIntent(intent);
+        close = intent.getExtras().getBoolean("close");
+
+        if(close == true){
+            mediaPlayer.release();
+            mediaPlayer = null;
+            this.finish();
+        }
+        else if(close == false){
+            mediaPlayer.start();
+            this.onResume();
+        }
+    }
+    public void dismiss(){
+        mediaPlayer.release();
+        mediaPlayer = null;
+        this.moveTaskToBack(true);
+    }
+    public void snooze(){
+        mediaPlayer.pause();
+        this.moveTaskToBack(true);
     }
 }
